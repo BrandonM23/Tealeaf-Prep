56d0c39165a55ae93175ab310a247cf4b131dcd8
BrandonM23 <BJMellusi@gmail.com>
2015-08-26 22:51:09 -0400

    Intro Comp Easy Quiz 3

---
+++ IntroCompQ3Ex2-3.rb
@@ -0,0 +1,4 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+flintstones.push("Dino", "Hoppy")
+#flintstones.push("Dino").push("Hoppy") #push returns array so we can chain
+#flintstones.concat(%w(Dino Hoppy)) #concat adds an array rather than one item
